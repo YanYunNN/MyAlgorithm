357fa2c9c9db0af7aa4a96f41384e4475f33ae72
xcai <yanyunnnx@163.com>
2020-08-19 19:27:08 +0800

    增加多个锁的demo

--- Thread/src/com.yanyun.thread/practice/JOLDemo.java
+++ Thread/src/com.yanyun.thread/practice/JOLDemo.java
@@ -9,13 +9,44 @@ import org.openjdk.jol.info.ClassLayout;
  * @description 打印一个对象
  */
 public class JOLDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        /**
+         * 等待初始化的重量锁加载，为了展示偏向锁
+         */
+        Thread.sleep(5000);
+
         Object o = new Object();
         String printable = ClassLayout.parseInstance(o).toPrintable();
         System.out.println(printable);
 
+        /**
+         * 轻量级锁
+         */
         synchronized (o) {
             System.out.println(ClassLayout.parseInstance(o).toPrintable());
         }
+        /**
+         * 锁竞争，重量级锁
+         */
+        new Thread(() -> {
+            synchronized (o) {
+                System.out.println(Thread.currentThread().getThreadGroup().getName());
+                System.out.println(ClassLayout.parseInstance(o).toPrintable());
+            }
+            System.out.println(ClassLayout.parseInstance(o).toPrintable());
+        }, "Thread-1").start();
+        new Thread(() -> {
+            synchronized (o) {
+                System.out.println(Thread.currentThread().getThreadGroup().getName());
+                System.out.println(ClassLayout.parseInstance(o).toPrintable());
+            }
+        }, "Thread-2").start();
+
+
+        /**
+         * 无锁
+         */
+        Thread.sleep(3000);
+        System.out.println(ClassLayout.parseInstance(o).toPrintable());
     }
 }
