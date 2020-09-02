7d2ebc7939276ebce9118b818d074ed1fb38015a
xcai <yanyunnnx@163.com>
2020-09-02 20:58:46 +0800

    jstack

--- ALGORITHM/src/com.yanyun.code/leetcode/A15_ThreesNumZero.java
+++ ALGORITHM/src/com.yanyun.code/leetcode/A15_ThreesNumZero.java
@@ -15,24 +15,22 @@ public class A15_ThreesNumZero {
 
     public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
         //边界
-        int length = nums.length;
-        if (nums == null || length < 3) {
-            return res;
-        }
+        if (nums == null || len < 3) return res;
         //排序减重，a<=b<=c
         Arrays.sort(nums);
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < len; i++) {
             // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
             if (nums[i] > 0) break;
             if (i > 0 && nums[i] == nums[i - 1]) continue; //去重，找下一轮
             //双指针
             int L = i + 1;
-            int R = length - 1;
+            int R = len - 1;
             while (L < R) {
                 int sum = nums[i] + nums[L] + nums[R];
                 if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[L] + nums[R]));
+                    res.add(Arrays.asList(nums[i], nums[L], nums[R]));
                     while (L < R && nums[L] == nums[L + 1]) L++; // 去重
                     while (L < R && nums[R] == nums[R - 1]) R--; // 去重
                     L++;
