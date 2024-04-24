5a7483f4e35903283b995820ba7a820bcc24dae7
YanYunNNX <YanYunNNX@163.com>
2024-04-24 14:18:24 +0000

    feat: leetcode

--- leetcode/src/main/java/medium/Q053_maxSubArray.java
+++ leetcode/src/main/java/medium/Q053_maxSubArray.java
@@ -16,28 +16,30 @@ public class Q053_maxSubArray {
         }
         return ans;
 
-        //标准DP
-//        int[] dp = new int[nums.length]; //dp[i]为以i为结尾的子序列和的最大值
-//        dp[0] = nums[0];
-//        int ans = nums[0];
-//        for (int i = 1; i < nums.length; i++) {
-//            // if (dp[i - 1] > 0) {
-//            //     dp[i] = dp[i - 1] + nums[i];
-//            // } else {
-//            //     dp[i] = nums[i];
-//            // }
-//            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
-//            ans = Math.max(dp[i], ans);
-//        }
-//        return ans;
+    }
 
+    public int maxSubArray1(int[] nums) {
+        //标准DP
+        int[] dp = new int[nums.length]; //dp[i]为以i为结尾的子序列和的最大值
+        dp[0] = nums[0];
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // if (dp[i - 1] > 0) {
+            //     dp[i] = dp[i - 1] + nums[i];
+            // } else {
+            //     dp[i] = nums[i];
+            // }
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            ans = Math.max(dp[i], ans);
+        }
+        return ans;
         //优化空间 DP，因为i只与i-1和当前i有关系，可以省去dp new空间
-//        int res = nums[0];
-//        for (int i = 1; i < nums.length; i++) {
-//            nums[i] += Math.max(nums[i - 1], 0);
-//            res = Math.max(res, nums[i]);
-//        }
-//        return res;
+        // int res = nums[0];
+        // for (int i = 1; i < nums.length; i++) {
+        //     nums[i] += Math.max(nums[i - 1], 0);
+        //     res = Math.max(res, nums[i]);
+        // }
+        // return res;
     }
     //leetcode submit region end(Prohibit modification and deletion)
 }
